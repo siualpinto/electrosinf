6422aad657687b11654e177abbae7285fcfe72b2
siualpinto <lsmoreira_77@hotmail.com>
2015-11-17 15:19:56 +0000

    php

---
+++ site/pages/produto.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>EletroSinf Produto</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Other Css -->
+    <link href="../css/top-bar.css" rel="stylesheet">
+    <link href="../css/produto.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">EletroSinf</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default btn-sm">Procurar</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#"> <span class="glyphicon glyphicon-share" aria-hidden="true"></span> </a>
+                    </li>
+                    <li>
+                        <a href="shopping-cart.html"> <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> </a>
+                    </li>
+                    <li>
+                        <a href="#"> <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+
+        <div class="col-md-6">
+            <div class="col-md-8 col-md-offset-2">
+                <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+            </div>
+
+            <div class="col-md-12">
+                <div class="col-md-2 product-mini">
+                    <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+                </div>
+                <div class="col-md-2 product-mini">
+                    <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+                </div>
+                <div class="col-md-2 product-mini">
+                    <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+                </div>
+                <div class="col-md-2 product-mini">
+                    <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+                </div>
+                <div class="col-md-2 product-mini">
+                    <img src="../img/maquinalavar.png" class="img-responsive" alt="Maquina Lavar AEG">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-md-6">
+            <div class="product-title">
+                <h3>MAQUINA LAVAR ROUPA AEG L 68280 FL</h3>
+            </div>
+
+            <div class="product-info-container">
+                <div class="product-info-item">
+                    <strong>Preço: </strong> €300,00
+                </div>
+
+                <div class="product-info-item">
+                    <strong>Marca: </strong> AEG
+                </div>
+
+                <div class="product-info-item">
+                    <strong>Classe: </strong> A++
+                </div>
+
+                <div class="product-info-item">
+                    <strong>Garantia: </strong> 2 anos
+                </div>
+
+                <div class="product-info-item">
+                    <strong>Disponibilidade: </strong> 13 produtos
+                </div>
+
+                <div class="product-info-item">
+                    <button type="button" class="btn btn-success">Adicionar ao carrinho</button>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="col-md-8 product-specs">
+            <div>
+                <h4>Mais Informações </h4>
+            </div>
+
+            <div>
+                <p>A máquina de lavar roupa AEG L 68280 FL assegura um tratamento delicado da roupa graças ao tambor com estrutura de favos, que faz a roupa deslizar por uma fina película de água. Com capacidade para 7 kg, é muito económica, integrando a classe de eficiência energética A++, e oferece resultados de centrifugação perfeitos a 1.400 rpm. Testada para 20 anos de vida útil, destaca-se ainda pelos tempos de programa curtos, pela capacidade de lavagem a baixas temperaturas, pelo seu sistema automático de quantidade e pelo contador do volume de água. Outras características: regulação de espuma; sistema Watercontrol (monitoriza a entrada de água, deteta fugas, assim como água no tabuleiro inferior); bloqueio de emergência; 8 programas de lavagem. A EletroSinf destaca:tambor com estrutura de favos; classe A++; 20 anos de vida útil; sistema automático de quantidade; lavagem a temperaturas baixas; contador do volume de água; regulação de espuma; sistema Watercontrol; bloqueio de energência; 8 programas.
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+
+                        <tbody>
+                            <tr class="product-spec-toggle">
+                                <td class="col-md-3"><strong>Especificações</strong></td>
+                                <td class="text-right"><a id="product-spec-text">Lista completa </a><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Referência</strong></td>
+                                <td>5538275</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Capacidade</strong></td>
+                                <td>7 Kg</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Centrifugação</strong></td>
+                                <td>1000 rpm</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Classe Energética</strong></td>
+                                <td>A++</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Ecrã Integrado</strong></td>
+                                <td>Sim</td>
+                            </tr>
+                            <tr class="product-spec-hidden">
+                                <td><strong>Tipo Ecrã</strong></td>
+                                <td>LED</td>
+                            </tr>
+                            <tr class="product-spec-hidden">
+                                <td><strong>Tipo de motor</strong></td>
+                                <td>Universal</td>
+                            </tr>
+                            <tr class="product-spec-hidden">
+                                <td><strong>Áqua-Stop</strong></td>
+                                <td>Sim</td>
+                            </tr>
+                            <tr class="product-spec-hidden">
+                                <td><strong>Tipo de cuba</strong></td>
+                                <td>Inox</td>
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-md-8 product-related">
+            <div>
+                <h4>Relacionados </h4>
+            </div>
+            <div class="col-md-12 product-related-info">
+                <div class="col-md-3 product-related-mini">
+                    <div>
+                        <img src="../img/ml1.png" class="img-responsive" alt="Maquina Lavar AEG">
+                    </div>
+                    <div class="product-related-title">
+                        <small><strong>Máquina de Lavar Roupa MIELE Wda 100</strong></small>
+                    </div>
+                    <div class="product-related-specs">
+                        <small>7 Kg / 1400 rpm / A++</small>
+                    </div>
+                    <div class="product-related-price">
+                        €599,99
+                    </div>
+                </div>
+                <div class="col-md-3 product-related-mini">
+                    <div>
+                        <img src="../img/ml2.png" class="img-responsive" alt="Maquina Lavar AEG">
+                    </div>
+                    <div class="product-related-title">
+                        <small><strong>Máquina de Lavar Roupa Siemens WM10Q408ES</strong></small>
+                    </div>
+                    <div class="product-related-specs">
+                        <small>8 Kg / 1000 rpm / A+++</small>
+                    </div>
+                    <div class="product-related-price">
+                        €499,99
+                    </div>
+                </div>
+                <div class="col-md-3 product-related-mini">
+                    <div>
+                        <img src="../img/ml3.png" class="img-responsive" alt="Maquina Lavar AEG">
+                    </div>
+                    <div class="product-related-title">
+                        <small><strong>Máquina de Lavar Roupa CANDY GV 158 TWC3/1</strong></small>
+                    </div>
+                    <div class="product-related-specs">
+                        <small>Alto rendimento / A++</small>
+                    </div>
+                    <div class="product-related-price">
+                        €399,99
+                    </div>
+                </div>
+                <div class="col-md-3 product-related-mini">
+                    <div>
+                        <img src="../img/ml4.png" class="img-responsive" alt="Maquina Lavar AEG">
+                    </div>
+                    <div class="product-related-title">
+                        <small><strong>Máquina de Lavar Roupa LG F12BoTDW</strong></small>
+                    </div>
+                    <div class="product-related-specs">
+                        <small>8 Kg / 1200 rpm / A++</small>
+                    </div>
+                    <div class="product-related-price">
+                        €499,99
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/produto.js"></script>
+    <!-- Other JS -->
+
+
+</body>
+
+</html>
