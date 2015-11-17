7234fe41b24673d0649f04f017d5a142065a7482
siualpinto <lsmoreira_77@hotmail.com>
2015-11-17 16:22:02 +0000

    merge

--- site/pages/homepage.php
+++ site/pages/homepage.php
@@ -141,22 +141,14 @@
         </div>
 
 
-
-<<<<<<< HEAD
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="../js/bootstrap.min.js"></script>
-        <script src="../js/homepage.js"></script>
-        <!-- Other JS -->
-=======
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/bootstrap.min.js"></script>
-    <!-- Other JS -->
-    <script src="../js/header.js"></script>
->>>>>>> origin/master
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="../js/bootstrap.min.js"></script>
+            <!-- Other JS -->
+            <script src="../js/header.js"></script>
+            <script src="../js/homepage.js"></script>
+
 
 
 </body>
